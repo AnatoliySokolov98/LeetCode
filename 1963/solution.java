847c2fe1e38f179027629356afbb0a5b10ed185d
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2024-04-22 18:40:24 -0700

    1963

--- 1963/solution.java
+++ 1963/solution.java
@@ -1,46 +1,20 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] edge : edges) {
-            int x = edge[0];
-            int y = edge[1];
-            graph.putIfAbsent(x, new ArrayList<>());
-            graph.putIfAbsent(y, new ArrayList<>());
-            graph.get(x).add(y);
-            graph.get(y).add(x);
-        }
-        Deque<Integer> bfs = new ArrayDeque<>();
-        bfs.add(source);
-        Set<Integer> visited = new HashSet<>();
-        visited.add(source);
-
-        while (!bfs.isEmpty()) {
-            int node = bfs.pollFirst();
-            if (node == destination) {
-                return true;
-            }
-            if (!graph.containsKey(node)) {
-                continue;
-            }
-            for (int child : graph.get(node)) {
-                if (!visited.contains(child)) {
-                    visited.add(child);
-                    bfs.add(child);
+    public int minSwaps(String s) {
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[')
+                left++;
+            else {
+                if (right == left) {
+                    res++;
+                    left++;
+                } else {
+                    right++;
                 }
             }
         }
-        return false;
+        return res;
     }
 }
-
-// time O(V + E)
-// space O(V + E)
