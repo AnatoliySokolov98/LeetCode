f1dd7461d4710eb27fab2f38a463ec46694cebef
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2024-06-18 18:07:38 -0700

    1482

--- 1482/solution.ts
+++ 1482/solution.ts
@@ -1,20 +1,46 @@
-function numSubseq(nums: number[], target: number): number {
-  const MOD = 10 ** 9 + 7;
-  let l = 0;
-  let r = nums.length - 1;
-  let res = 0;
-  nums.sort((a, b) => a - b);
-  const powers = [1];
-  for (let i = 1; i < nums.length; i++) {
-    powers.push((powers[i - 1] * 2) % MOD);
+function minDays(bloomDay: number[], m: number, k: number): number {
+  if (m * k > bloomDay.length) {
+    return -1;
   }
+  let l = Infinity;
+  let r = 0;
+  for (const flower of bloomDay) {
+    l = Math.min(l, flower);
+    r = Math.max(r, flower);
+  }
+  let res = r;
   while (l <= r) {
-    if (nums[l] + nums[r] > target) {
-      r--;
+    let p = Math.floor((l + r) / 2);
+    if (canMakeBouquets(bloomDay, m, k, p)) {
+      res = p;
+      r = p - 1;
     } else {
-      res = (res + powers[r - l]) % MOD;
-      l++;
+      l = p + 1;
     }
   }
   return res;
 }
+const canMakeBouquets = (
+  bloomDay: number[],
+  m: number,
+  k: number,
+  day: number
+) => {
+  let res = 0;
+  let curr = 0;
+  for (const flower of bloomDay) {
+    if (flower <= day) {
+      curr++;
+      if (curr == k) {
+        res++;
+        curr = 0;
+        if (res == m) {
+          return true;
+        }
+      }
+    } else {
+      curr = 0;
+    }
+  }
+  return false;
+};
