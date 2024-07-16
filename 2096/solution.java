86ad90ffc7962d9bc6ab0ebdf413286c0cbf9dcb
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2024-07-16 10:11:03 -0700

    2096

--- 2096/solution.java
+++ 2096/solution.java
@@ -1,33 +1,41 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
-
 class Solution {
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        for (int num : nums1) {
-            set1.add(num);
+    String startPath = "";
+    String endPath = "";
+    StringBuilder path = new StringBuilder();
+
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        traverse(root, startValue, destValue);
+        int i = 0;
+        while (i < startPath.length() && i < endPath.length() && startPath.charAt(i) == endPath.charAt(i)) {
+            i++;
         }
-        Set<Integer> set2 = new HashSet<>();
-        List<Integer> res1 = new ArrayList<>();
-        List<Integer> res2 = new ArrayList<>();
-        for (int num : nums2) {
-            set2.add(num);
+        int stepsUp = startPath.length() - i;
+        StringBuilder result = new StringBuilder();
+        for (int j = 0; j < stepsUp; j++) {
+            result.append('U');
         }
-        for (int item : set1) {
-            if (!set2.contains(item)) {
-                res1.add(item);
-            }
+        return result.toString() + endPath.substring(i);
+    }
+
+    private void traverse(TreeNode node, int startValue, int destValue) {
+        if (node.val == startValue) {
+            startPath = path.toString();
         }
-        for (int item : set2) {
-            if (!set1.contains(item)) {
-                res2.add(item);
-            }
+
+        if (node.val == destValue) {
+            endPath = path.toString();
         }
-        List<List<Integer>> finalList = new ArrayList<>();
-        finalList.add(res1);
-        finalList.add(res2);
-        return finalList;
+
+        if (node.left != null) {
+            path.append('L');
+            traverse(node.left, startValue, destValue);
+            path.deleteCharAt(path.length() - 1);
+        }
+
+        if (node.right != null) {
+            path.append('R');
+            traverse(node.right, startValue, destValue);
+            path.deleteCharAt(path.length() - 1);
+        }
     }
 }
