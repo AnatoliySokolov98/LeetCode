9af5d16d06298f208819218d1d066472cdbcf7d9
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2024-08-17 21:08:41 -0700

    264

--- 0264/solution.go
+++ 0264/solution.go
@@ -1,18 +1,30 @@
 package main
 
-func isAnagram(s string, t string) bool {
-	if len(s) != len(t) {
-		return false
-	}
-	counts := make(map[byte]int)
-	for i := 0; i < len(s); i++ {
-		counts[s[i]]++
-		counts[t[i]]--
-	}
-	for _, value := range counts {
-		if value != 0 {
-			return false
+func nthUglyNumber(n int) int {
+	uglyNums := make([]int, n)
+	uglyNums[0] = 1
+	twos := 0
+	threes := 0
+	fives := 0
+
+	for i := 1; i < n; i++ {
+		uglyNums[i] = min(uglyNums[twos]*2, min(uglyNums[threes]*3, uglyNums[fives]*5))
+		if uglyNums[i] == uglyNums[twos]*2 {
+			twos++
+		}
+		if uglyNums[i] == uglyNums[threes]*3 {
+			threes++
+		}
+		if uglyNums[i] == uglyNums[fives]*5 {
+			fives++
 		}
 	}
-	return true
+	return uglyNums[n-1]
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
