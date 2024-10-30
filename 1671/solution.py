98ce114f1f74eb2439de81207c6d92c442f173ba
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2024-10-29 19:02:37 -0700

    1671

--- 1671/solution.py
+++ 1671/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minimumMountainRemovals(self, nums: List[int]) -> int:
+    def minimumMountainRemovals(self, nums: list[int]) -> int:
         lefts = [1] * len(nums)
         for i in range(len(nums)):
             for j in range(i):
