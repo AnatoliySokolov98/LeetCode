3a626e21dca3e6fb9fddfe85d85dedcddee99aec
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2026-01-19 10:52:46 -0800

    973

--- 0973/solution.rs
+++ 0973/solution.rs
@@ -1,28 +1,21 @@
-use std::collections::HashMap;
+use std::collections::BinaryHeap;
+
 
 impl Solution {
-    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let k: usize = k as usize;
-        let l = nums.len();
-        let mut buckets: Vec<Vec<i32>> = vec![vec![]; l + 1];
-        let mut counts: HashMap<i32, i32> = HashMap::new();
-        for num in nums {
-            *counts.entry(num).or_default() += 1;
-        }
-
-        for (key, val) in counts {
-            buckets[val as usize].push(key);
-        }
-
-        let mut res: Vec<i32> = Vec::with_capacity(k);
-        for bucket in buckets.iter().rev() {
-            for &item in bucket {
-                res.push(item);
-                if res.len() == k {
-                    return res;
-                }
+    pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+        let k = k as usize;
+        let mut heap: BinaryHeap<(i32, usize)> = BinaryHeap::new();
+        
+        for (i, point) in points.iter().enumerate() {
+            let dist = point[0].pow(2) + point[1].pow(2);
+            heap.push((dist, i));
+            if heap.len() > k {
+                heap.pop();
             }
         }
-        return res;
+        
+        heap.into_iter()
+            .map(|(_, index)| points[index].clone())
+            .collect()
     }
 }
